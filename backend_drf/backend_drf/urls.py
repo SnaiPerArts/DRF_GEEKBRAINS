cf73557bac4e29bcb643d7c12e8ebcd32a0050f3
Denis Prusakov <snaiper.arts@gmail.com>
2022-04-26 20:54:00 +0400

    Lesson6

--- backend_drf/backend_drf/urls.py
+++ backend_drf/backend_drf/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from users.views import UserInfoModelViewSet
 from todo.views import NoteModelViewSet, ProjectModelViewSet
 
@@ -27,4 +29,8 @@ router.register('projects', ProjectModelViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
